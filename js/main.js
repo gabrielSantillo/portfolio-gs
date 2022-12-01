04ead14e716ebe6079a9d528c7fdde4cda85ec30
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-01 15:03:35 -0700

    Adding logic to the menu nav bar to show and hide

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ let navMenu = document.getElementById('nav-menu'),
 
 /* MENU SHOW */
 /* VALIDATE IF CONSTANT EXISTS */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* MENU HIDDEN */
+/* VALIDATE IF CONSTANT EXISTS */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /* REMOVE MENU MOBILE */
 
