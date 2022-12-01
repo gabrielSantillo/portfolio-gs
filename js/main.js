6efd3ac2cb5e245eaea08c40cfa5f43f61ff8784
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-01 14:56:47 -0700

    Starting to add js to make the menu show and hide

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* MENU SHOW Y HIDDEN */
+let navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* MENU SHOW */
+/* VALIDATE IF CONSTANT EXISTS */
+
+/* REMOVE MENU MOBILE */
+
+/* ACCORDION SKILLS */
+
+/* QUALIFICATIONS TABS */
+
