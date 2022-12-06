006e2040505bdafbbc041916661838165acf73bc
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-06 16:15:49 -0700

    Adding the function that will change the theme of the page by each click on the theme button

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@ window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER  ====================*/
 function scrollHeader(){
-    const nav = document.getElementById('header');
+    let nav = document.getElementById('header');
     if(this.scrollY >= 80) {
         nav.classList.add('scroll-header');
     } else {
@@ -150,3 +150,33 @@ function scrollUp(){
     }
 }
 window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/
+let themeButton = document.getElementById('theme-button')
+let darkTheme = 'dark-theme'
+let iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+let selectedTheme = localStorage.getItem('selected-theme')
+let selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+let getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+let getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
