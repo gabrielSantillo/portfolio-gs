c307843330f7021899036dd000cdbe59ccf681f3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-12-06 16:02:26 -0700

    Adding a scroll button to the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,3 +126,27 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER  ====================*/
+function scrollHeader(){
+    const nav = document.getElementById('header');
+    if(this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    } 
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollUp(){
+    let scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) {
+       scrollUp.classList.add('show-scroll');  
+    }
+    else {
+        scrollUp.classList.remove('show-scroll')
+    }
+}
+window.addEventListener('scroll', scrollUp)
